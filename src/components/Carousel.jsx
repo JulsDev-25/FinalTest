48a12c2590a454995c39d7dda79d3e022f252428
JulsDev-25 <julesloic9@gmail.com>
2025-03-04 15:15:09 +0100

    Mise forme du caroussel 2

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,54 +1,196 @@
 import React from "react";
+import { Button, Grid2 as Grid, IconButton } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination, Autoplay } from "swiper/modules";
 import "../App.css";
 import { Box } from "@mui/material";
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled';
 
 const Carousel = () => {
-  return (
-    <Swiper
-      modules={[Pagination, Autoplay]}
-      slidesPerView={1}
-      pagination={{ clickable: true, }}
-      autoplay={{
-        delay: 4000,
-        disableOnInteraction: false, 
-      }}
-      loop={true}
-      style={{ width: "100vw", height: "100vh", backgroundColor: "black" }}
-    >
-      <SwiperSlide>
-        <Box id="b1" sx={{width: "100%", height: "100%"}}>
-            
-        </Box>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Box id="b2" sx={{width: "100%", height: "100%"}}>
+    return (
+        <Swiper
+            modules={[Pagination, Autoplay]}
+            slidesPerView={1}
+            pagination={{ clickable: true, }}
+            autoplay={{
+                delay: 4000,
+                disableOnInteraction: false,
+            }}
+            loop={true}
+            style={{ width: "100vw", height: "100vh", backgroundColor: "black" }}
+        >
+            <SwiperSlide>
+                <Box id="b1" sx={{ width: "100%", height: "100%" }}>
+                    <Grid sx={{ height: "100%", mx: "50px" }}>
+                        <Grid
+                            size={6}
+                            sx={{
+                                height: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                color: "white",
+                                justifyContent: "center",
+                                alignItems: "flex-start",
+                                gap: "15px"
+                            }}
+                        >
+                            <p style={{ color: "gray" }}>TO DO POR SOLO S/19.90 AL MES</p>
+                            <h1>Last major séries actuel</h1>
+                            <Button size="large" sx={{ mt: "15px", padding: "10px 15px", fontWeight: "bold", color: "white", background: "#f17404", textTransform: "none" }}>
+                                Contacter nous
+                            </Button>
+                        </Grid>
+                        <Grid
+                            size={6}
+                            sx={{ height: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}
 
-        </Box>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Box id="b3" sx={{width: "100%", height: "100%"}}>
+                        >
+                            <IconButton >
+                                <PlayCircleFilledIcon size="large" />
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </SwiperSlide>
+            <SwiperSlide>
+                <Box id="b2" sx={{ width: "100%", height: "100%" }}>
+                    <Grid sx={{ height: "100%", mx: "50px" }}>
+                        <Grid
+                            size={6}
+                            sx={{
+                                height: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                color: "white",
+                                justifyContent: "center",
+                                alignItems: "flex-start",
+                                gap: "15px"
+                            }}
+                        >
+                            <p style={{ color: "gray" }}>TO DO POR SOLO S/19.90 AL MES</p>
+                            <h1>Last major séries actuel</h1>
+                            <Button size="large" sx={{ mt: "15px", padding: "10px 15px", fontWeight: "bold", color: "white", background: "#f17404", textTransform: "none" }}>
+                                Contacter nous
+                            </Button>
+                        </Grid>
+                        <Grid
+                            size={6}
+                            sx={{ height: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}
 
-        </Box>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Box id="b4" sx={{width: "100%", height: "100%"}}>
+                        >
+                            <IconButton >
+                                <PlayCircleFilledIcon size="large" />
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </SwiperSlide>
+            <SwiperSlide>
+                <Box id="b3" sx={{ width: "100%", height: "100%" }}>
+                    <Grid sx={{ height: "100%", mx: "50px" }}>
+                        <Grid
+                            size={6}
+                            sx={{
+                                height: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                color: "white",
+                                justifyContent: "center",
+                                alignItems: "flex-start",
+                                gap: "15px"
+                            }}
+                        >
+                            <p style={{ color: "gray" }}>TO DO POR SOLO S/19.90 AL MES</p>
+                            <h1>Last major séries actuel</h1>
+                            <Button size="large" sx={{ mt: "15px", padding: "10px 15px", fontWeight: "bold", color: "white", background: "#f17404", textTransform: "none" }}>
+                                Contacter nous
+                            </Button>
+                        </Grid>
+                        <Grid
+                            size={6}
+                            sx={{ height: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}
 
-        </Box>
-      </SwiperSlide>
-      <SwiperSlide>
-        <Box id="b5" sx={{width: "100%", height: "100%"}}>
+                        >
+                            <IconButton >
+                                <PlayCircleFilledIcon size="large" />
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </SwiperSlide>
+            <SwiperSlide>
+                <Box id="b4" sx={{ width: "100%", height: "100%" }}>
+                    <Grid sx={{ height: "100%", mx: "50px" }}>
+                        <Grid
+                            size={6}
+                            sx={{
+                                height: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                color: "white",
+                                justifyContent: "center",
+                                alignItems: "flex-start",
+                                gap: "15px"
+                            }}
+                        >
+                            <p style={{ color: "gray" }}>TO DO POR SOLO S/19.90 AL MES</p>
+                            <h1>Last major séries actuel</h1>
+                            <Button size="large" sx={{ mt: "15px", padding: "10px 15px", fontWeight: "bold", color: "white", background: "#f17404", textTransform: "none" }}>
+                                Contacter nous
+                            </Button>
+                        </Grid>
+                        <Grid
+                            size={6}
+                            sx={{ height: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}
 
-        </Box>
-      </SwiperSlide>
-    </Swiper>
-  );
+                        >
+                            <IconButton >
+                                <PlayCircleFilledIcon size="large" />
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </SwiperSlide>
+            <SwiperSlide>
+                <Box id="b5" sx={{ width: "100%", height: "100%" }}>
+                    <Grid sx={{ height: "100%", mx: "50px" }}>
+                        <Grid
+                            size={6}
+                            sx={{
+                                height: "100%",
+                                display: "flex",
+                                flexDirection: "column",
+                                color: "white",
+                                justifyContent: "center",
+                                alignItems: "flex-start",
+                                gap: "15px"
+                            }}
+                        >
+                            <p style={{ color: "gray" }}>TO DO POR SOLO S/19.90 AL MES</p>
+                            <h1>Last major séries actuel</h1>
+                            <Button size="large" sx={{ mt: "15px", padding: "10px 15px", fontWeight: "bold", color: "white", background: "#f17404", textTransform: "none" }}>
+                                Contacter nous
+                            </Button>
+                        </Grid>
+                        <Grid
+                            size={6}
+                            sx={{ height: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}
+
+                        >
+                            <IconButton >
+                                <PlayCircleFilledIcon size="large" />
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </SwiperSlide>
+        </Swiper>
+    );
 };
 
 const style = {
-    
+
 }
 export default Carousel;
---
+++ src/components/Presentation.jsx
@@ -0,0 +1,22 @@
+import { Grid2 as Grid } from '@mui/material';
+import React from 'react';
+
+const Presentation = () => {
+    return (
+        <Grid size={12}>
+            <Grid
+                size= {{xs: 12, md:6}}
+            >
+                <p>POR QUE ELEGIRNOS</p>
+                <h1></h1>
+            </Grid>
+            <Grid
+                size= {{xs: 12, md:6}}
+            >
+
+            </Grid>
+        </Grid>
+    );
+}
+
+export default Presentation;
